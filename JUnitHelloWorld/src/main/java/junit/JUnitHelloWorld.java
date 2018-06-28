0ca2d74d4a0414a7d6ab0f7768612c5ec7c3c45b
Peter Asp <peter.aspus@gmail.com>
2018-06-28 19:37:34 +0200

    EL-137 Change on feature-branch

--- JUnitHelloWorld/src/main/java/junit/JUnitHelloWorld.java
+++ JUnitHelloWorld/src/main/java/junit/JUnitHelloWorld.java
@@ -1,7 +1,7 @@
 package junit;
 
 public class JUnitHelloWorld {
-// Comment here again & again & again
+// Comment here again & again & again & again
 	public boolean isGreater(int num1, int num2){
 		return num1 == num2;
 	}
